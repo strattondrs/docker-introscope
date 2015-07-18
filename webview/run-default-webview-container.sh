c2966544382a723772cdadc460c77fbe4d36ecb4
grogu01 <Guenter.Grossberger@ca.com>
2015-07-17 22:06:16 -0700

    adapted run scripts

--- webview/run-default-webview-container.sh
+++ webview/run-default-webview-container.sh
@@ -1,4 +1,15 @@
 #!/bin/sh
 # This runs a container in default mode. Note that this expects that the sieglstefan/introscope-em already is started
 
-docker run -d --name introscope-webview -p 8080:8080 --link introscope-em:em ggrossbe/introscope-webview-10.0
+sudo=sudo
+unamestr=`uname`
+
+if [[ "$unamestr" == 'Darwin' ]]; then
+	sudo=''
+fi
+
+$sudo docker run -d \
+	-p 8082:8080 \
+	--link=apm-em:em \
+	--name=apm-webview \
+	apm-docker-registry.ca.com:5000/apm-webview
