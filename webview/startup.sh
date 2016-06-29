51b48300fdceccad4a72c1d8182e9d660fe9e4a2
grogu01 <Guenter.Grossberger@ca.com>
2016-06-29 09:54:50 +0200

    replaced old link variables and made EM connection configurable via env variables

--- webview/startup.sh
+++ webview/startup.sh
@@ -7,8 +7,15 @@ if [ ! -f IntroscopeWebView-orig.properties ]
 		cp ${INTROSCOPE_HOME}/config/IntroscopeWebView.properties IntroscopeWebView-orig.properties
 fi
 cp IntroscopeWebView-orig.properties ${INTROSCOPE_HOME}/config/IntroscopeWebView.properties
-sed -i s/EM_HOST_MARKER/${EM_PORT_5001_TCP_ADDR}/g ${INTROSCOPE_HOME}/config/IntroscopeWebView.properties
-sed -i s/EM_PORT_MARKER/${EM_PORT_5001_TCP_PORT}/g ${INTROSCOPE_HOME}/config/IntroscopeWebView.properties
+sed -i s/EM_HOST_MARKER/${EM_HOST}/g ${INTROSCOPE_HOME}/config/IntroscopeWebView.properties
+sed -i s/EM_PORT_MARKER/${EM_PORT}/g ${INTROSCOPE_HOME}/config/IntroscopeWebView.properties
+sed -i s/{EM_HOST}:8081/${EM_HOST}:${EM_WEB_PORT}/g ${INTROSCOPE_HOME}/config/IntroscopeWebView.properties
+
+# activate Java agent
+sed -i "s#lax.nl.java.option.additional=#lax.nl.java.option.additional=-javaagent:./product/webview/agent/wily/Agent.jar -Dcom.wily.introscope.agentProfile=./product/webview/agent/wily/core/config/IntroscopeAgent.profile -Dcom.wily.introscope.wilyForWilyPrefix=com.wily -Djetty.home=./ #" ${INTROSCOPE_HOME}/Introscope_WebView.lax
+sed -i s/EM_HOST_MARKER/${EM_HOST}/g ${INTROSCOPE_HOME}/product/webview/agent/wily/core/config/IntroscopeAgent.profile
+sed -i s/EM_PORT_MARKER/${EM_PORT}/g ${INTROSCOPE_HOME}/product/webview/agent/wily/core/config/IntroscopeAgent.profile
+
 
 # configure the heap
 if [ "${HEAP_XMX}" == "**DEFAULT**" ] ; then
@@ -19,7 +26,7 @@ if [ -n "${HEAP_XMX}" ] ; then
 fi
 
 # give EM time to start up
-sleep 15s
+sleep 15
 
 # now we correctly set everything and startup the enterprise manager.
 ${INTROSCOPE_HOME}/Introscope_WebView
