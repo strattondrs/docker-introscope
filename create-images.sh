44eef6ccb21861b721a1f137f78341a43be04b24
grogu01 <Guenter.Grossberger@ca.com>
2018-05-04 11:28:10 -0700

    added Mac case

--- create-images.sh
+++ create-images.sh
@@ -1,5 +1,14 @@
 #!/bin/sh
 path=$(pwd)
+OS="`uname`"
+case $OS in
+  'Darwin')
+    SED_OPTIONS="${SED_OPTIONS}"
+    ;;
+    *)
+
+    ;;
+esac
 
 INTROSCOPE_VERSION=10.7.0.45
 INTROSCOPE_HOTFIX=10.7.0HF03
@@ -10,15 +19,15 @@ ERRORS=false
 INTROSCOPE_TAR=introscope${INTROSCOPE_VERSION}linuxAMD64.tar
 INTROSCOPE_BIN=introscope${INTROSCOPE_VERSION}linuxAMD64.bin
 
-sed -i '' "s/INTROSCOPE_VERSION=[0-9\.]*/INTROSCOPE_VERSION=${INTROSCOPE_VERSION}/g" $path/*/build-image.sh
-sed -i '' "s/INTROSCOPE_VERSION=[0-9\.]*/INTROSCOPE_VERSION=${INTROSCOPE_VERSION}/g" $path/*/Dockerfile
-sed -i '' "s/INTROSCOPE_HOTFIX=[0-9\.HF\-]*/INTROSCOPE_HOTFIX=${INTROSCOPE_HOTFIX}/g" $path/*/build-image.sh
-sed -i '' "s/INTROSCOPE_HOTFIX_ZIP=.*/INTROSCOPE_HOTFIX_ZIP=${INTROSCOPE_HOTFIX_ZIP}/g" $path/*/build-image.sh
-sed -i '' "s/INTROSCOPE_HOTFIX=[0-9\.HF\-]*/INTROSCOPE_HOTFIX=${INTROSCOPE_HOTFIX}/g" $path/*/Dockerfile
-sed -i '' "s/\(docker build.*:\)[0-9\.HF\-]*/\1${IMAGE_VERSION_TAG}/g" $path/*/build-image.sh
-sed -i '' "s/\(docker tag[^\:]*:\)[0-9\.HF\-]*/\1${IMAGE_VERSION_TAG}/g" $path/*/build-image.sh
-sed -i '' "s/\(image: .*:\)[0-9\.HF\-]*/\1${IMAGE_VERSION_TAG}/g" $path/docker-compose.yml
-sed -i '' "s/IMAGE_VERSION_TAG=[0-9\.HF\-]*/IMAGE_VERSION_TAG=${IMAGE_VERSION_TAG}/g" push-images.sh
+sed ${SED_OPTIONS} "s/INTROSCOPE_VERSION=[0-9\.]*/INTROSCOPE_VERSION=${INTROSCOPE_VERSION}/g" $path/*/build-image.sh
+sed ${SED_OPTIONS} "s/INTROSCOPE_VERSION=[0-9\.]*/INTROSCOPE_VERSION=${INTROSCOPE_VERSION}/g" $path/*/Dockerfile
+sed ${SED_OPTIONS} "s/INTROSCOPE_HOTFIX=[0-9\.HF\-]*/INTROSCOPE_HOTFIX=${INTROSCOPE_HOTFIX}/g" $path/*/build-image.sh
+sed ${SED_OPTIONS} "s/INTROSCOPE_HOTFIX_ZIP=.*/INTROSCOPE_HOTFIX_ZIP=${INTROSCOPE_HOTFIX_ZIP}/g" $path/*/build-image.sh
+sed ${SED_OPTIONS} "s/INTROSCOPE_HOTFIX=[0-9\.HF\-]*/INTROSCOPE_HOTFIX=${INTROSCOPE_HOTFIX}/g" $path/*/Dockerfile
+sed ${SED_OPTIONS} "s/\(docker build.*:\)[0-9\.HF\-]*/\1${IMAGE_VERSION_TAG}/g" $path/*/build-image.sh
+sed ${SED_OPTIONS} "s/\(docker tag[^\:]*:\)[0-9\.HF\-]*/\1${IMAGE_VERSION_TAG}/g" $path/*/build-image.sh
+sed ${SED_OPTIONS} "s/\(image: .*:\)[0-9\.HF\-]*/\1${IMAGE_VERSION_TAG}/g" $path/docker-compose.yml
+sed ${SED_OPTIONS} "s/IMAGE_VERSION_TAG=[0-9\.HF\-]*/IMAGE_VERSION_TAG=${IMAGE_VERSION_TAG}/g" push-images.sh
 
 if [ "$ERRORS" = false ] ; then
 
